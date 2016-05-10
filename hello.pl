275e24dc1f4384eebb197a25ed7e778029b4f361
Dane <Danesprite@gmail.com>
2016-05-10 11:19:13 +1000

    Added some clauses and more comments

--- hello.pl
+++ hello.pl
@@ -1,4 +1,9 @@
 % http://www.swi-prolog.org/FAQ/UnixExe.html
+% For UNIX executable: swipl -o hello -g hello -c hello.pl
 hello :-
+    hello(10, 0).
+hello(N, N) :- halt.
+hello(N, C) :-
     format('Hello world!~n'),
-    halt.
+    NewC is C + 1,
+    hello(N, NewC).
